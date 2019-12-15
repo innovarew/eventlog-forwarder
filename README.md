f7d77bcf1ca1db8914cf80ff3a3dfa4ca77624e5
innovarew <48243982+innovarew@users.noreply.github.com>
2019-12-15 07:23:52 +0100

    Update eventlog-forwarder

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# eventlog-forwarder
+### eventlog-forwarder: A simple Windows Eventlog forwarder to Syslog using TCP SSL.
+
+Perform the following steps to setup eventlog-forwarding to the Syslog server:
+
+- Configure the Syslog Server IP at [eventlog-forwarder-config.bat](eventlog-forwarder-config.bat)
+- Check the log forwarding works invoking: [eventlog-forwarder.bat](eventlog-forwarder.bat)
+- Once the configuration is working schedule a daily task: `eventlog-forwarder.bat daily`
+
+#### Usage
+
+~~~
+C:\eventlog-forwarder> eventlog-forwarder.bat usage
+
+usage: eventlog-forwarder.bat [usage|daily|disable|status]
+- usage: show eventlog-forwarder.bat usage
+- daily: schedule daily eventlog-forwarder.bat task
+- disable: delete eventlog-forwarder.bat daily schedule task
+- status: show eventlog-forwarder.bat status
+- otherwise, run the eventlog-forwarder
+
+usage: eventlog-forwarder Server-IP Protocol Port AppName LogNames LogCycle LogRange # @version v1.5
+@copyright #Innovarew (c) Copyright 2013. All rights reserved.
+~~~
+
---
+++ eventlog-forwarder-config.bat
@@ -0,0 +1,56 @@
+::
+:: @file eventlog-forwarder-config.bat
+:: @version v1.4
+::
+:: @url https://innovarew.appspot.com/
+:: @author Innovarew <innovarew.solutions (at) gmail (dot) com>
+:: @copyright (C) 2013 Innovarew Solutions. All Rights Reserved.
+:: @license Proprietary and confidential
+::     Unauthorized copying of this file, via any medium is strictly prohibited.
+:: Contact us if you are interested in using our products and services.
+::
+:: Changelog:
+::
+:: [dd/mm/yyyy]        [author]
+:: [brief description]
+::
+:: 21/01/2013        innovare.solutions
+:: v1.0 Initial version
+::
+:: 15/02/2013        innovare.solutions
+:: v1.1 Added TCP TLS support
+::
+:: 10/03/2013        innovare.solutions
+:: v1.2 Added config eventlog-forwarder-config.bat
+::
+:: 08/04/2013        innovare.solutions
+:: v1.3 Support forwarding several LogNames="Security,System" etc.
+::
+:: 14/04/2013        innovare.solutions
+:: v1.4 Support forwarding regular application log files.
+::
+
+:: IMPORTANT Remember to add the correspnding TLS certificate to the store with
+:: certutil -addstore -enterprise -f "Root" /path/to/certificate/file
+
+:: Configurable options
+
+:: Example of Syslog UPD/514 configuration
+::set server=172.20.17.140
+::set protocol="UDP"
+::set port=514
+
+:: Example of Syslog TCP/SSL configuration
+set server=172.20.17.140
+set protocol="TCPwithTLS"
+set port=1999
+
+:: keep logs from last week (remove logs older than 7 days)
+set logcycle=7
+:: forward logs from today (send logs from 1 day back from today)
+set logrange=1
+
+:: Last, forward the different Event and App Logs to the Syslog Server
+%eventlogfwd% %server% %protocol% %port% "EventLog" "Security" %logcycle% %logrange%
+%eventlogfwd% %server% %protocol% %port% "AppLog" "/path/to/app.log" %logcycle% %logrange%
+
